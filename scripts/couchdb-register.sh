133ca20a5a0a6833b78d767d83d26f16bb3baf28
Michael Yuen <michael.yuen@mac.com>
2016-10-26 21:15:47 +0100

    Compose file & script that accepts master node name

--- scripts/couchdb-register.sh
+++ scripts/couchdb-register.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# couchdb-primary
+MASTER=${1}
+
+if [[ -z "${MASTER}" ]]
+then
+  echo "Usage : ${0} <master>"
+  exit 1
+fi
+
 HOSTNAME=$(hostname)
 LOCAL_IP=$(grep ${HOSTNAME} /etc/hosts | cut -f1)
 
-curl -X PUT "http://couchdb-primary:5986/_nodes/${HOSTNAME}@${LOCAL_IP}" -d {}
+curl -X PUT "http://${MASTER}:5984/_nodes/${HOSTNAME}@${LOCAL_IP}" -d {}
+
+
+
